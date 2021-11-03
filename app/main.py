6e587e67a9a1bb2f87e8dc5daec22fd21e2b51f4
Richard Hsu <rhsu@alumni.cmu.edu>
2021-11-03 16:54:14 -0700

    pika project baseline

--- app/main.py
+++ app/main.py
@@ -14,15 +14,14 @@ from utils import get_base_url, allowed_file, and_syntax
     port may need to be changed if there are multiple flask servers running on same server
     comment out below three lines of code when ready for production deployment
 '''
-port = 12345
-base_url = get_base_url(port)
-app = Flask(__name__, static_url_path=base_url+'static')
-
+port = 12346
+#base_url = get_base_url(port)
+#app = Flask(__name__, static_url_path=base_url+'static')
 '''
     cv scaffold code
     uncomment below line when ready for production deployment
 '''
-# app = Flask(__name__)
+app = Flask(__name__)
 
 UPLOAD_FOLDER = 'images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -30,22 +29,22 @@ app.config['MAX_CONTENT_LENGTH'] = 32 * 1024 * 1024
 
 # set up yolo net for prediction
 # you will need to change names, weights, and configuration files.
-names_path = os.path.join('yolo', 'wolf.names')
+names_path = os.path.join('yolo', 'pika.names')
 LABELS = open(names_path).read().strip().split("\n")
 COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
 
-weights_path = os.path.join('yolo', 'wolf_best.weights')
-cfg_path = os.path.join('yolo', 'wolf.cfg')
+weights_path = os.path.join('yolo', 'pika_best.weights')
+cfg_path = os.path.join('yolo', 'pika.cfg')
 net = get_yolo_net(cfg_path, weights_path)
 
 
-#@app.route('/')
-@app.route(base_url)
+@app.route('/')
+#@app.route(base_url)
 def home():
     return render_template('home.html')
 
-#@app.route('/', methods=['POST'])
-@app.route(base_url, methods=['POST'])
+@app.route('/', methods=['POST'])
+#@app.route(base_url, methods=['POST'])
 def home_post():
     # check if the post request has the file part
     if 'file' not in request.files:
@@ -65,8 +64,8 @@ def home_post():
         return redirect(url_for('results', filename=filename))
 
 
-#@app.route('/uploads/<filename>')
-@app.route(base_url + '/uploads/<filename>')
+@app.route('/uploads/<filename>')
+#@app.route(base_url + '/uploads/<filename>')
 def results(filename): 
     image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     image = cv2.imread(image_path)
@@ -94,10 +93,10 @@ def results(filename):
     else:
         found = False
         # replace 'Objects' with whatever you are trying to detect
-        return render_template('results.html', labels='No Objects', old_filename=filename, filename=filename) 
+        return render_template('results.html', labels='no cereals', old_filename=filename, filename=filename) 
 
-#@app.route('/files/<path:filename>')
-@app.route(base_url + '/files/<path:filename>')
+@app.route('/files/<path:filename>')
+#@app.route(base_url + '/files/<path:filename>')
 def files(filename):
     return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
 
@@ -106,8 +105,8 @@ if __name__ == "__main__":
     coding center code
     '''
     # IMPORTANT: change the cocalcx.ai-camp.org to the site where you are editing this file.
-    website_url = 'coding.ai-camp.org'
-    print(f"Try to open\n\n    https://{website_url}" + base_url + '\n\n')
+    #website_url = 'coding.ai-camp.org'
+    #print(f"Try to open\n\n    https://{website_url}" + base_url + '\n\n')
 
     # remove debug=True when deploying it
     app.run(host = '0.0.0.0', port=port, debug=True)
